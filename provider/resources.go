7cfd7e49b0ca725d1d15db9b5c4e9bed972407d7
Radu Mocanu <radu_mocanu2000@yahoo.com>
2025-05-13 09:59:38 +0300

    doc: added logo url, regenerated sdks

--- provider/resources.go
+++ provider/resources.go
@@ -139,7 +139,7 @@ func Provider() tfbridge.ProviderInfo {
 		//
 		// You may host a logo on a domain you control or add an PNG logo (100x100) for your package
 		// in your repository and use the raw content URL for that file as your logo URL.
-		LogoURL: "",
+		LogoURL: "https://raw.githubusercontent.com/ionos-cloud/pulumi-ionoscloud/refs/heads/main/.github/LOGO_IONOS_Blue_RGB.png",
 		// PluginDownloadURL is an optional URL used to download the Provider
 		// for use in Pulumi programs
 		// e.g. https://github.com/org/pulumi-provider-name/releases/download/v${VERSION}/
