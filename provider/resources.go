6a096a1cb148a4d9e1e57cdc9d999d1d13d85bd8
cristi <cristian.mihai.guran@ionos.com>
2026-04-15 12:04:25 +0300

    fix: remaining linter issues

--- provider/resources.go
+++ provider/resources.go
@@ -143,7 +143,8 @@ func Provider() tfbridge.ProviderInfo {
 		//
 		// You may host a logo on a domain you control or add an PNG logo (100x100) for your package
 		// in your repository and use the raw content URL for that file as your logo URL.
-		LogoURL: "https://raw.githubusercontent.com/ionos-cloud/pulumi-ionoscloud/refs/heads/main/.github/LOGO_IONOS_Blue_RGB.png",
+		LogoURL: "https://raw.githubusercontent.com/ionos-cloud/pulumi-ionoscloud" +
+			"/refs/heads/main/.github/LOGO_IONOS_Blue_RGB.png",
 		// PluginDownloadURL is an optional URL used to download the Provider
 		// for use in Pulumi programs
 		PluginDownloadURL: "github://api.github.com/ionos-cloud",
